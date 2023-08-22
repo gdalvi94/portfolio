1ce55948ce186a1daa1f4c52655ea77d5cb1c321
gdalvi94 <gayatridalvi94@gmail.com>
2023-08-22 15:02:46 -0400

    Updated config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["example.com"], // Add domains for external images
+    domains: ["gdalvi94.github.io"], // Add domains for external images
     unoptimized: true, // Unoptimize images for faster development builds
   },
   // Other configuration options
