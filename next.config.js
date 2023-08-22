2cc6109686271208d3f55cf41dca757ee7dc2f22
gdalvi94 <gayatridalvi94@gmail.com>
2023-08-22 14:26:19 -0400

    config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// next.config.js
+module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ["example.com"], // Add domains for external images
+    unoptimized: true, // Unoptimize images for faster development builds
+  },
+  // Other configuration options
 };
-
-module.exports = nextConfig;
